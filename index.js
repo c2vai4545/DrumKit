e3251ac8e185f78e0999a5a701485f43c179b4a7
c2vai4545 <c2vai2003@me.com>
2024-02-09 19:07:33 -0300

    Adding Listeners
    Adding eventListener to each button and adding alert so we can test it

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 /* archivo JavaScript para index.html */
+const botones  = document.querySelectorAll("button");
+
+// Agregar un evento de clic a cada botón
+botones.forEach(function (btn) {
+    btn.addEventListener('click', function() {
+    alert("I got clicked");
+    });
+});
