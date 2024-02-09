df63f32297a0267fdc945c4ac8739ff8c7d7fad2
c2vai4545 <c2vai2003@me.com>
2024-02-09 20:26:11 -0300

    configuracion de botones
    Se añaden las configuraciones de los botones para emitir el sonido al hacer click en ellos y se agregan las imagenes de fondo de cada uno de ellos.

--- index.js
+++ index.js
@@ -1,9 +1,72 @@
 /* archivo JavaScript para index.html */
-const botones  = document.querySelectorAll("button");
+var numeroBotones = document.querySelectorAll(".drum").length; //obtengo el número de botones en la página
+var audioW = new Audio("./sounds/kick-bass.mp3");
+var audioA = new Audio("./sounds/tom-1.mp3")
+var audioS = new Audio("./sounds/tom-2.mp3");
+var audioD = new Audio("./sounds/tom-4.mp3");
+var audioJ = new Audio("./sounds/snare.mp3");
+var audioK = new Audio("./sounds/tom-3.mp3");
+var audioL = new Audio("./sounds/crash.mp3");
+// var botonW = document.querySelector(".w.drum");
+// var botonA = document.querySelector(".a.drum");
+// var botonS = document.querySelector(".s.drum");
+// var botonD = document.querySelector(".d.drum");
+// var botonJ = document.querySelector(".j.drum");
+// var botonK = document.querySelector(".k.drum");
+// var botonL = document.querySelector(".l.drum");
 
-// Agregar un evento de clic a cada botón
-botones.forEach(function (btn) {
-    btn.addEventListener('click', function() {
-    alert("I got clicked");
+for (var i = 0;  i < numeroBotones; i++) {
+    //agrego un listener en cada boton
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+        var buttonInnerHTML = this.innerHTML;
+        
+            switch (buttonInnerHTML) {
+                case "w":
+                    audioW.play();
+                    break;
+                case "a":
+                    audioA.play();
+                    break;
+                case "s":
+                    audioS.play();
+                    break;
+                case "d":
+                    audioD.play();
+                    break;
+                case "j":
+                    audioJ.play();
+                    break;
+                case "k":
+                    audioK.play();
+                    break;
+                case "l":
+                    audioL.play();
+                    break;
+                default:
+                    console.log(buttonInnerHTML);
+                    break;
+            }
     });
-});
+}
+
+// botonW.addEventListener("click", function(){
+//     audioW.play();
+// });
+// botonA.addEventListener("click", function(){
+//     audioA.play();
+// });
+// botonS.addEventListener("click", function(){
+//     audioS.play();
+// });
+// botonD.addEventListener("click", function(){
+//     audioD.play();
+// });
+// botonJ.addEventListener("click", function(){
+//     audioJ.play();
+// });
+// botonK.addEventListener("click", function(){
+//     audioK.play();
+// });
+// botonL.addEventListener("click", function(){
+//     audioL.play();
+// });
